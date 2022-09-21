ee95ff906c264f1c24b9633bcdec82a122b173ff
igolubev <igolubevapp@gmail.com>
2022-09-21 10:16:42 +0300

    startup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,1 @@
-fn main() {
-  let my_number = 2;
-
-  match my_number {
-    1 => println!("один"),
-    2 => println!("два"),
-    3 => println!("три"),
-    _ => println!("другое"),
-  }
-}
+fn main() {}
