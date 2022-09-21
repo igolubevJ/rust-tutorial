bdd8debd9bdea7153ee78a63a9e83ccfbc7b5de3
igolubev <igolubevapp@gmail.com>
2022-09-21 09:47:51 +0300

    create

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+fn main() {
+  let my_number = 2;
+
+  match my_number {
+    1 => println!("один"),
+    2 => println!("два"),
+    3 => println!("три"),
+    _ => println!("другое"),
+  }
+}
